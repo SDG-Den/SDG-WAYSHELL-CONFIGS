ffeafdf52b142726c1597a35d3f1e188c5aa32c6
SDGDen <sdgden@sdgcloud.nl>
2026-07-10 09:29:43 +0200

    updated

--- install.sh
+++ install.sh
@@ -6,17 +6,12 @@ unipkg install any sdg-wayshell
 # set working directory
 WORKDIR=$HOME/.cache/SDG-PKG/sdg-wayshell-conf
 
-# install configs with subdirectory structure preserved
-mkdir -p $HOME/.config/SDG-WAYSHELL-CONFIGS
-cp -r $WORKDIR/config/wayshellconf/* $HOME/.config/SDG-WAYSHELL-CONFIGS/
 
-# install monocle configs
-mkdir -p $HOME/.config/SDG-MONOCLE
-cp -r $WORKDIR/config/SDG-MONOCLE/* $HOME/.config/SDG-MONOCLE/
+# install configs
+cp -r $WORKDIR/config/* $HOME/.config
 
-# install monocle scripts
-mkdir -p $HOME/.local/SDG-MONOCLE
-cp -r $WORKDIR/local/SDG-MONOCLE/* $HOME/.local/SDG-MONOCLE/
+# install scripts
+cp -r $WORKDIR/local/* $HOME/.local
 
 # install docs and tips
 mkdir -p $HOME/.local/docs
